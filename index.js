510a7bfc081ad37840ff28e0a419d865fed2b36d
Daniel Teel <daniel.teel.2@us.af.mil>
2022-05-21 14:25:54 -0600

    Added .html generation

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ let title=process.argv[2];
 let file=process.argv[3];
 if (!file) file='test.txt';
 
+
 const contents=fs.readFileSync(file).toString();
 
 const tokens=Tokenizer.tokenize(contents);
 
 let mqf=Parser.parse(tokens);
-mqf={title, ...mqf};
+const htmlMQF=JSON.stringify({title, ...mqf}, null).replaceAll("'", "\\'");
 
-fs.writeFileSync(file+'.json', JSON.stringify(mqf, null, '  '));
+const header=fs.readFileSync('./data/shellstart.txt');
+const footer=fs.readFileSync('./data/shellend.txt');
+fs.writeFileSync(file+'.html', header);
+fs.appendFileSync(file+'.html', htmlMQF);
+fs.appendFileSync(file+'.html', footer);
